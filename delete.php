7e5419378ab3a737f6283b92298294efc921f23a
alouiabdo <147491362+alouiabdo@users.noreply.github.com>
2024-04-15 12:52:47 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,9 +5,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     try {
         $db = new PDO("mysql:host=localhost;dbname=test","root","");
         $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        $stm = "DELETE * FROM `tableone` WHERE id=$id";
-        $db>exec($stm);
-        echo "Record deleted successfully";
+        $db->query("DELETE FROM `tableone` WHERE id=$id");
+        // $db>exec($stm);
+        echo "Record deleted successfully ".$id;
     } catch(PDOException $e) {
         echo "UPDATED not successfully: " . $e->getMessage();
         }
